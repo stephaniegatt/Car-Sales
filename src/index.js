47b6562a5db675a2480e91257a86b7fb3ac91592
Stephanie Gatt <stephanielynngatt@gmail.com>
2020-07-15 10:41:53 -0400

    set up redux, added action features and reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { featuresReducer } from "./reducers/featuresReducer";
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+// import AddedFeature from './components/AddedFeature';
+
+const store = createStore(featuresReducer);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
